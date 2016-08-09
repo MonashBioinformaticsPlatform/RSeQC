6eb1d781afa4a2a92187a447b49a22c3c4077a54
serine <tskiril@gmail.com>
2016-08-09 14:48:05 +0000

    moved files around to make this more python package like. working on rseqc to include other "modules"

--- rseqc/modules/read_dist/read_dist.py
+++ rseqc/modules/read_dist/read_dist.py
@@ -36,15 +36,6 @@ from qcmodule import BED
 from qcmodule import SAM
 from qcmodule import bam_cigar
 
-__author__ = "Liguo Wang"
-__copyright__ = "Copyleft"
-__credits__ = []
-__license__ = "GPL"
-__version__="2.6.4"
-__maintainer__ = "Liguo Wang"
-__email__ = "wang.liguo@mayo.edu"
-__status__ = "Production"
-
 def cal_size(list):
 	'''calcualte bed list total size'''
 	size=0
