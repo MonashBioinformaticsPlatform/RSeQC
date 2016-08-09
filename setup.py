a47472776631b3626b880eee34d702311fbb905f
serine <tskiril@gmail.com>
2016-08-09 13:16:21 +0000

    cleaned it up

--- setup.py
+++ setup.py
@@ -1,50 +1,31 @@
-"""
-Setup script for RSeQC -- Comprehensive QC package for RNA-seq data
-"""
+#!/usr/bin/env python
+# -*- coding: iso-8859-15 -*-
 
-import sys, os, platform, glob
-from distutils.core import setup
-from setuptools import *
+import os
+from setuptools import setup
 
+setup(
+    name = "RSeQC",
+    version = "2.6.4",
+    author = "Liguo Wang",
+    author_email ="wangliguo78@gmail.com",
+    description = ("RNA-seq QC Package"),
+    long_description=read('README'),
+    license = "GPLv2",
+    keywords='RNA-seq, RNAseq, QC, metrics',
+    url = "https://github.com/MonashBioinformaticsPlatform/RSeQC",
+    scripts = ['scripts/rseqc'],
+    install_requires = [
+        'numpy',
+        'cython>=0.17',
+        'pysam',
+        'bx-python'
+        ], 
+    packages=['RSeQC']
+    )
 
-if sys.version_info[0] != 2 or sys.version_info[1] < 7:
-	print >> sys.stderr, "ERROR: RSeQC requires Python 2.7"
-	sys.exit()
-	
-
-def main():
-	setup(  name = "RSeQC",
-            version = "2.6.4",
-            packages = find_packages( 'lib' ),
-            package_dir = { '': 'lib' },
-            package_data = { '': ['*.ps'] },
-            scripts = glob.glob( "scripts/*.py"),
-            ext_modules = [],
-            py_modules = [ 'psyco_full' ],
-            test_suite = 'nose.collector',
-            setup_requires = ['nose>=0.10.4','cython>=0.12'],
-            author = "Liguo Wang",
-			author_email ="wangliguo78@gmail.com",
-			platforms = ['Linux','MacOS'],
-			requires = ['cython (>=0.17)'],
-			install_requires = ['cython>=0.17','pysam','bx-python','numpy'], 
-            description = "RNA-seq QC Package",
-            url = "http://rseqc.sourceforge.net/",
-            zip_safe = False,
-            dependency_links = [],
-			classifiers=[
-              'Development Status :: 5 - Production/Stable',
-              'Environment :: Console',
-              'Intended Audience :: Science/Research',
-              'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-              'Operating System :: MacOS :: MacOS X',
-              'Operating System :: POSIX',
-              'Programming Language :: Python',
-              'Topic :: Scientific/Engineering :: Bio-Informatics',
-              ],
-			keywords='RNA-seq, QC',
-        )
-
-
-if __name__ == "__main__":
-	main()
+#platforms = ['Linux','MacOS'],
+#classifiers=[
+#    "Development Status :: 3 - Alpha",
+#    "Topic :: Utilities",
+#    "License :: OSI Approved :: BSD License",
